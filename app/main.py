040b16518ff15d6de4e2d032cf38b8d88e475384
Rahul N S <rahulgowda843138@gmail.com>
2026-06-26 07:04:57 +0530

    text git

---
+++ app/main.py
@@ -0,0 +1,20 @@
+from fastapi import FastAPI
+
+from routers import company,job
+
+app=FastAPI()
+
+app.include_router(company.router)
+app.include_router(job.router)
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+@app.get("/about")
+def read_about():
+    return {"about": "this is about page"}
+
+@app.get("/contact")
+def read_contact():
+    return {"contact": "Contact Mr Rahul"}
