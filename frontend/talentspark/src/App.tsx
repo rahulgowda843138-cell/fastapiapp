3cd21306b2cb4c3855d445660a96a1fd7e835f0d
Rahul N S <rahulgowda843138@gmail.com>
2026-06-29 10:57:03 +0530

    talentspark

---
+++ frontend/talentspark/src/App.tsx
@@ -0,0 +1,14 @@
+import Welcome from "./components/welcome";
+import NavBar from "./components/NavBar";
+import Footer from "./components/Footer";
+function App(){
+  return(
+    <>
+    <NavBar/>
+    <Welcome/>
+    <Footer/>
+    </>
+  )
+
+}
+export default App
---
+++ frontend/talentspark/src/components/Welcome.tsx
@@ -0,0 +1,6 @@
+function Welcome(){
+    return(
+        <h1>Welcome to talentspark</h1>
+    )
+}
+export default Welcome
