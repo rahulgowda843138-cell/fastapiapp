fe0e2accd0bf707f2fde7f07b0ff5876db504729
Rahul N S <rahulgowda843138@gmail.com>
2026-07-01 12:35:33 +0530

    edit_words

--- frontend/talentspark/src/Services/CompanyService.ts
+++ frontend/talentspark/src/Services/CompanyService.ts
@@ -8,3 +8,28 @@ export async function getCompanies (): Promise<Company[]>{
     return response.data;
 }
     
+export async function getCompanyById (id: number): 
+    Promise<Company>{
+        const response = await axios.get(`${API_BASE_URL}/
+        company/${id}`);
+        return response.data;
+}
+
+export async function createCompany (company: Company): 
+    Promise<Company>{
+        const response = await axios.post(`${API_BASE_URL}/
+        company`, company);
+        return response.data;
+}
+export async function updateCompany (id: number, company: Company): 
+    Promise<Company>{
+        const response = await axios.put(`${API_BASE_URL}/
+        company/${id}`, company);
+        return response.data;
+}
+export async function deleteCompany (id: number):
+    Promise<Company>{
+        const response = await axios.delete(`${API_BASE_URL}/
+        company/${id}`);
+        return response.data;
+}
--- frontend/talentspark/src/types/job.ts
+++ frontend/talentspark/src/types/job.ts
@@ -1,8 +1,8 @@
-interface job{
+interface Job{
     title:String;
     salary:number;
     description:String;
     company_id:number;
     
 }
-export type {job}
+export type {Job}
