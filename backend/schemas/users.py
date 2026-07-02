fecbd153e9f73753ec32feccc443845e93f55099
Rahul N S <rahulgowda843138@gmail.com>
2026-07-02 16:22:25 +0530

    login

--- backend/schemas/users.py
+++ backend/schemas/users.py
@@ -12,6 +12,12 @@ class UserCreate(UserBase):
 
 class UserResponse(UserBase):
     id: int
-
+    name: str
+    email: str
+    role: str
     class Config:
-        orm_mode = True
+        from_attributes = True
+
+class Login_User(BaseModel):
+    email: str
+    password: str
